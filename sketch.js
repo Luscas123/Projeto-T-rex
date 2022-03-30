cb7792f8aa2ab681ee5f4856972ec2db843bdb0c
Luscas123 <102263815+Luscas123@users.noreply.github.com>
2022-03-30 20:36:27 -0300

    Chão infinito, e tudo em função.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,54 @@
-function setup(){
-  createCanvas(600,200);
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload() {
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  trex_collided = loadImage("trex_collided.png");
+
+  groundImage = loadImage("ground2.png")
 }
 
-function draw(){
-  background("white");
-  drawSprites();
+function setup() {
+createCanvas(600, 200);
+
+
+trex = createSprite(50,160,20,50);
+trex.addAnimation("running", trex_running);
+trex.scale = 0.5;
+  
+invisibleGround = createSprite(50,195,600,15);
+invisibleGround.visible = false;
+
+ground = createSprite(200,180,400,20);
+ground.addImage("ground",groundImage);
+ground.x = ground.width /2;
+ground.velocityX = -4;
+  
+}
+
+function draw() {
+background("white");
+
+jump();
+
+
+infinityground();
+
+
+
+trex.collide(invisibleGround);
+drawSprites();
+}
+
+function jump(){
+  if (keyDown("space")&& trex.collide(invisibleGround)) {
+    trex.velocityY = -10;
+  }
+  trex.velocityY = trex.velocityY + 0.8;
+}
+
+function infinityground(){
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
 }
